effafa1db8581e92c0a71b07d5ed14d78f734ccf
webratte <webratte@users.noreply.github.com>
2022-11-10 08:53:43 +0100

    Languages
    start to make it multi lingual

---
+++ languages.js
@@ -0,0 +1,38 @@
+function changeLanguage() {
+	browserLang = window.navigator.language;
+	browserLang = browserLang.slice(0, 2).toLowerCase();
+		if (browserLang == "de") {
+			//german
+			strSendMemo = "    Memo senden    ";
+			strWasSent = " Memo wurde gesendet ";
+			strPreviousUpdate = " Vorherige Änderung ";
+			strEditMemo = "   Memo bearbeiten  ";
+			strLast = "Letzte";
+			strThis = "Diese";
+			strUpdate = "Änderung";
+			strInfoMessage = "Memo wurde geändert durch";
+			}
+		else if (browserLang == "en") {
+			//english
+			strSendMemo = "   Send Memo   ";
+			strWasSent = " Memo was sent ";
+			strPreviousUpdate = " Previous Update ";
+			strEditMemo = "    Edit Memo    ";
+			strLast = "Last";
+			strThis = "This";
+			strUpdate = " Update ";
+			strInfoMessage = "Memo was updated by";
+			}
+		else {
+			//fallback
+			strSendMemo = "   Send Memo   ";
+			strWasSent = " Memo was sent ";
+			strPreviousUpdate = " Previous Update ";
+			strEditMemo = "    Edit Memo    ";
+			strLast = "Last";
+			strThis = "This";
+			strUpdate = " Update ";
+			strInfoMessage = "Memo was updated by";
+		}
+};
+

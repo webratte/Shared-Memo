cf0115be0d6ead93476fcab48b7b9dcc52d485e5
webratte <webratte@users.noreply.github.com>
2022-11-12 15:17:44 +0100

    added french
    using online translater

--- languages.js
+++ languages.js
@@ -3,10 +3,10 @@ function changeLanguage() {
 	browserLang = browserLang.slice(0, 2).toLowerCase();
 		if (browserLang == "de") {
 			//german
-			strSendMemo = "    Memo senden    ";
-			strWasSent = " Memo wurde gesendet ";
-			strPreviousUpdate = " Vorherige Änderung ";
-			strEditMemo = "   Memo bearbeiten  ";
+			strSendMemo = "Memo senden";
+			strWasSent = "Memo wurde gesendet";
+			strPreviousUpdate = "Vorherige Änderung";
+			strEditMemo = "Memo bearbeiten";
 			strLast = "Letzte";
 			strThis = "Diese";
 			strUpdate = "Änderung";
@@ -15,21 +15,33 @@ function changeLanguage() {
 			}
 		else if (browserLang == "en") {
 			//english
-			strSendMemo = "   Send Memo   ";
-			strWasSent = " Memo was sent ";
-			strPreviousUpdate = " Previous Update ";
-			strEditMemo = "    Edit Memo    ";
+			strSendMemo = "Send Memo";
+			strWasSent = "Memo was sent";
+			strPreviousUpdate = "Previous Update";
+			strEditMemo = "Edit Memo";
 			strLast = "Last";
 			strThis = "This";
 			strUpdate = " Update ";
 			strInfoMessage = "Memo was updated by";
 			}
+		else if (browserLang == "fr") {
+			//french
+			strSendMemo = "Envoyer mémo";
+			strWasSent = "Mémo a été envoyé";
+			strPreviousUpdate = "Mise à jour précédente";
+			strEditMemo = "Éditer le mémo";
+			strLast = "Dernier";
+			strThis = "Cette";
+			strUpdate = "Modification";
+			//strInfoMessage = "Le mémo a été mis à jour par";
+			strInfoMessage = "Memo was updated by";
+			}
 		else {
 			//fallback
-			strSendMemo = "   Send Memo   ";
-			strWasSent = " Memo was sent ";
-			strPreviousUpdate = " Previous Update ";
-			strEditMemo = "    Edit Memo    ";
+			strSendMemo = "Send Memo";
+			strWasSent = "Memo was sent";
+			strPreviousUpdate = "Previous Update";
+			strEditMemo = "Edit Memo";
 			strLast = "Last";
 			strThis = "This";
 			strUpdate = " Update ";

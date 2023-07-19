d4774e517bf692115f41ec7463523d6ece2e807a
webratte <webratte@users.noreply.github.com>
2023-07-19 07:40:15 +0200

    Store timestamp of incoming update

--- storage.js
+++ storage.js
@@ -1,9 +1,17 @@
+// store time if send-button is pressed
 function storeSentTime() {
 	window.CurrentTime = Date.now();
 	localStorage.setItem("sentUnixTime", window.CurrentTime);
 
 };
 
+// store time of incoming update
+function storeIncomingTime() {
+	localStorage.setItem("sentUnixTime", window.CurrentTime);
+	
+};
+
+// read time of already exist update
 function readUnixtime() {
 	storedSentTime = localStorage.getItem("sentUnixTime");
 	

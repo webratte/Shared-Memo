58253ddb63f6637e844de47a8f53fca2d525a896
webratte <webratte@users.noreply.github.com>
2023-08-09 21:33:02 +0200

    Update languages.js

--- languages.js
+++ languages.js
@@ -13,8 +13,9 @@ function changeLanguage() {
                strCancel = "Abbrechen";
                strSetTitle = "Titel ändern";
                strYourUpdate = "Ihr Entwurf:";
-               strIncomUpdate = "Gerade angekommene Änderung:";
-               strMessageDraft = "\nEs kam gerade ein eine Änderung an.\nIhr Entwurf wurde zum Vergleichen und anpassen gespeichert."
+               strIncomUpdate = "Gerade angekommene Änderung (" + window.updateUser +"):";
+               strMessageDraft = "\nEs kam gerade eine Änderung durch " + window.updateUser +" an.\nIhr Text wurde temporär als Entwurf gespeichert.\n\n!! ACHTUNG !!\nBeim Schließen der App geht er verloren, wenn das Memo nicht gesendet wird."
+               strDraftButton = "Entwurf anzeigen";
           }
           else if (browserLang == "fr") {
                //french
@@ -28,8 +29,10 @@ function changeLanguage() {
                strCancel = "Annuler";
                strSetTitle = "Changer le titre";
                strYourUpdate = "Votre Brouillon:";
-               strIncomUpdate = "Le changement vient d'arriver:";
+               strIncomUpdate = "Le changement vient d'arriver (" + window.updateUser +"):";
                strMessageDraft = "\nUn changement vient d'arriver.\nVotre brouillon a été enregistré pour comparaison et ajustement."
+               strMessageDraft = "\nUn changement de " + window.updateUser +" vient d'arriver.\nVotre texte a été temporairement enregistré en tant que brouillon.\n\n!! ATTENTIONNÉ !!\nIl sera perdu à la fermeture de l'application si le mémo n'est pas envoyé."
+               strDraftButton = "  Afficher le brouillon  ";
           }
           else {
                //fallback
@@ -43,12 +46,15 @@ function changeLanguage() {
                strCancel = "Cancel";
                strSetTitle = "Change Title";
                strYourUpdate = "Your Draft:";
-               strIncomUpdate = "Change just arrived:";
+               strIncomUpdate = "Change just arrived (" + window.updateUser +"):";
                strMessageDraft = "\nA change just arrived.\nYour draft has been saved for comparison and adjustment."
+               strMessageDraft = "\nA change by " + window.updateUser +" just arrived.\nYour text has been temporarily saved as a draft.\n\n!! ATTENTION !!\nIt will be lost when the app is closed if the memo is not sent."
+               strDraftButton = "  Show draft  ";
           }
      document.getElementById("updateButton").value=window.strSendMemo;
      document.getElementById("previous-button").value=window.strPreviousUpdate;
      document.getElementById("edit-button").value=window.strEditMemo;
      document.getElementById("setTitleButton").value=window.strSetTitle;
      document.getElementById('draftinfo').value=window.strMessageDraft;
+     document.getElementById('draft-button').value=window.strDraftButton;
 };

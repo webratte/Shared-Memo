84eabbafdaf2fb187e4c0aa5ddb3c08e1cfde459
webratte <webratte@users.noreply.github.com>
2023-09-02 08:06:04 +0200

    Some Codecleaning

--- history.js
+++ history.js
@@ -1,7 +1,7 @@
 function storeHistory() {
      arrHistory.push(window.prvLastSerial,window.prvTitle,window.prvMsg,window.prvUpdateUser,window.prvUpdateDate);
      if (arrHistory.length >=10) {
-     	    window.boolHistory = true
+     	    window.boolHistory = true;
      };
 }; 
 
@@ -10,16 +10,15 @@ function storeStartSerial() {
      	    window.startSerial = 0;
      }
      else{
-     	    window.startSerial = arrHistory[arrHistory.length - ((window.maxHistory+3) * 5)]
+     	    window.startSerial = arrHistory[arrHistory.length - ((window.maxHistory+3) * 5)];
      };
-     if (window.startSerial === null || window.startSerial === undefined || window.startSerial === 0) {    	    
+     if (window.startSerial === null || window.startSerial === undefined || window.startSerial === 0) {
      	    window.startSerial = window.tempStartSerial;
      };
      localStorage.setItem('lastSerial', window.startSerial);
 };
 
 function getStartValue() {
-          
 	  window.startSerial = localStorage.getItem('lastSerial');
      if (window.startSerial === null || window.startSerial === undefined) {
           window.startSerial = 0;
@@ -31,22 +30,22 @@ function callHistory() {
      document.getElementById('back-button').style='display: yes; float: right; border-radius: 5px;';
      document.getElementById('back-button').disabled = false;
      document.getElementById('forward-button').style='display: yes; float: left; border-radius: 5px;';
-     document.getElementById('forward-button').disabled = true;      
+     document.getElementById('forward-button').disabled = true;
      window.historyIndex = arrHistory.length -4;
-     window.showPrevious = true;  
+     window.showPrevious = true;
      document.getElementById('input').disabled = true;
-     document.getElementById('title').disabled = true;								      //deaktivate title input field
-     document.getElementById('previous-button').style='display: none'; 				//hide Previous Button
-     document.getElementById('edit-button').style='display: yes; float: right; border-radius: 5px;';		//make Edit Button visible
+     document.getElementById('title').disabled = true;
+     document.getElementById('previous-button').style='display: none';
+     document.getElementById('edit-button').style='display: yes; float: right; border-radius: 5px;';
      document.getElementById('updateButton').disabled = true;
-     window.msg = document.getElementById('input').value;     
-     window.tmpDraftMsg = window.msg;     
-     historyBack();			
+     window.msg = document.getElementById('input').value;
+     window.tmpDraftMsg = window.msg;
+     historyBack();
 };
-  
-function historyForward() { 
+
+function historyForward() {
      window.historyIndex = window.historyIndex + 5;
-     showHistory(); 
+     showHistory();
 };
 
 function historyBack() {
@@ -63,14 +62,14 @@ function showHistory() {
      };   
      if (window.historyIndex == ((arrHistory.length -4)-(window.maxHistory * 5))) {
      	    document.getElementById('back-button').disabled = true;
-     };       
+     };
      if (window.historyIndex <= arrHistory.length -14) {
      	    document.getElementById('forward-button').disabled = false;
      }
      else {
      	    document.getElementById('forward-button').disabled = true;
      };
-     document.getElementById('title').value = arrHistory[window.historyIndex];      
+     document.getElementById('title').value = arrHistory[window.historyIndex];
      document.getElementById('input').value = arrHistory[window.historyIndex+1];
      document.getElementById('output').innerHTML = strThis + ' ' + strUpdate +': ' + arrHistory[window.historyIndex+2] + '<br>' + '<small>' + arrHistory[window.historyIndex+3];
 };
--- storage.js
+++ storage.js
@@ -2,34 +2,31 @@
 function storeSentTime() {
      window.CurrentTime = Date.now();
      localStorage.setItem('sentUnixTime', window.CurrentTime);
-
 };
 
 // store time of incoming update
 function storeIncomingTime() {
-     localStorage.setItem('sentUnixTime', window.CurrentTime);
-	
+     localStorage.setItem('sentUnixTime', window.CurrentTime);	
 };
 
 // read time of already exist update
 function readUnixtime() {
      window.storedSentTime = localStorage.getItem('sentUnixTime');
-	
 };
 
 //handle if a new update arrives while memo is edited
 function setDraft() {
-     if (window.showPrevious === true) { 
+     if (window.showPrevious === true) {
           edit(); 
           window.draft = strYourUpdate + '\n' + window.tmpDraftMsg + '\n' + '\n' + strIncomUpdate + '\n' + window.msg;
      }
      else {
           window.draft = strYourUpdate + '\n' + document.getElementById('input').value + '\n' + '\n' + strIncomUpdate + '\n' + window.msg;
      };
-     
+
      document.getElementById('input').value = window.draft;
-     document.getElementById('updateButton').disabled = true;  
-     
+     document.getElementById('updateButton').disabled = true;
+
      // hide/deactivate elements
      cancelTitle();
      document.getElementById('input').style='display: none';
@@ -39,16 +36,14 @@ function setDraft() {
      
      // Show info
      document.getElementById('draftinfo').style='display: yes; max-width: calc(100% - 7px); text-align:center; border-radius: 5px;'; 
-     document.getElementById('draft-button').style='display: yes; border-radius: 5px;' ;
-     
+     document.getElementById('draft-button').style='display: yes; border-radius: 5px;';
 };
 
 function closeInfo() {
-     document.getElementById('draftinfo').style='display: none'; 
+     document.getElementById('draftinfo').style='display: none';
      document.getElementById('draft-button').style='display: none';
      document.getElementById('input').style='display: yes; max-width: calc(100% - 7px); border-radius: 5px;';
      document.getElementById('updateButton').style='display: yes; float: left; border-radius: 5px;';
      document.getElementById('previous-button').style='display: yes; float: right; border-radius: 5px;'; 
      document.getElementById('title').disabled = false;
-     
 };
